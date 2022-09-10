0ba9645a7cf5fc190de09a28443a7fa132414c3b
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-09-09 23:47:56 -0600

    fortune cookie added

--- server/controller.js
+++ server/controller.js
@@ -9,5 +9,14 @@ module.exports = {
       
         res.status(200).send(randomCompliment);
     }
+,
+    getFortune: (req, res) => {
+        const fortunes = ["You are going to be rich", "You are going through some important changes right now", "You will some day suck a little less then you do now at coding", "You will soon meet someone important", "Your poops are going to hurt after eating this chinese"]
+    
 
+    let randomIndex = Math.floor(Math.random() * fortunes.length)
+    let randomFortune = fortunes[randomIndex]
+
+    res.status(200).send(randomFortune)
+    }
 }
--- server/index.js
+++ server/index.js
@@ -7,8 +7,13 @@ app.use(cors());
 
 app.use(express.json());
 
-const { getCompliment } = require('./controller')
+app.listen(4000, () => console.log("Server running on 4000"));
+
+
+const {getCompliment} = require('./controller')
 
 app.get("/api/compliment", getCompliment);
 
-app.listen(4000, () => console.log("Server running on 4000"));
+const {getFortune} = require('./controller')
+
+app.get("/api/fortune", getFortune);
